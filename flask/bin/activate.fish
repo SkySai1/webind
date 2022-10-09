276ee272d88d7f186f0832954055b7c9a12ad1a3
sky@tinirog.ru <sky@tinirog.ru>
2022-10-09 00:05:30 +0000

    Добавлен скрипт реконфигруации

--- flask/bin/activate.fish
+++ flask/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/vic/webind-x86/flask"
+set -gx VIRTUAL_ENV "/home/vic/webind/flask"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
