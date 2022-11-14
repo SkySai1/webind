2f7b3fc6dad67f7c9dbd000b9b6f8e5977c90a42
Vitaly A <sky@tinirog.ru>
2022-11-14 23:28:20 +0300

    Добавлено управление зонами

---
+++ static/js/zonecontrol.js
@@ -0,0 +1,9 @@
+function setSerial(){
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+    today = yyyy + mm + dd+'01'
+    $('#zoneSerial').val(today)
+    rightshow('#new-zone')
+}
